1b18495791e071764dd4697fe30a41a5aad3bd54
fluk <100671860+Astaponfluk@users.noreply.github.com>
2022-11-13 20:20:51 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -61,6 +61,41 @@ function makeSound(key) {
       kick.play();
       break;
 
+      case "W":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "A":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "S":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+
+    case "D":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+
+    case "J":
+      var snare = new Audio('sounds/crash.mp3');
+      snare.play();
+      break;
+
+    case "K":
+      var crash = new Audio('sounds/kick-bass.mp3');
+      crash.play();
+      break;
+
+    case "L":
+      var kick = new Audio('sounds/snare.mp3');
+      kick.play();
+      break;
+
       case "ไ":
       var tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
@@ -92,6 +127,41 @@ function makeSound(key) {
       break;
 
     case "ส":
+      var kick = new Audio('sounds/snare.mp3');
+      kick.play();
+      break;
+
+      case '"':
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "ฤ":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "ฆ":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+
+    case "ฏ":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+
+    case "๋":
+      var snare = new Audio('sounds/crash.mp3');
+      snare.play();
+      break;
+
+    case "ษ":
+      var crash = new Audio('sounds/kick-bass.mp3');
+      crash.play();
+      break;
+
+    case "ศ":
       var kick = new Audio('sounds/snare.mp3');
       kick.play();
       break;
